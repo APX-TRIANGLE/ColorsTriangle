28a5853997ed3d7896211a9f17cfa6bb3596cfbd
DanielGiraldo <danielalberto.giraldo.contractor@bbva.com>
2019-09-14 11:48:29 -0500

    DAG - Fix: Ajustes en clase y optimización de la solución.

--- ColorsTriangle/src/colorstriangle/Kata.java
+++ ColorsTriangle/src/colorstriangle/Kata.java
@@ -5,61 +5,21 @@
  */
 package colorstriangle;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
  *
  * @author dgiraldo
  */
 public class Kata {
 
-    private static final Pattern patternLetraG = Pattern.compile("([RB])+");
-    private static final Pattern patternLetraB = Pattern.compile("([RG])+");
-    private static final Pattern patternLetraR = Pattern.compile("([GB])+");
+    public static char triangle(final String row) {
+        char[] caracteres = row.toCharArray();
+        int pos = (caracteres.length);
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void Kata(String[] args) {
-    }
-
-    public static char triangle(String cadenaEntrada) {
-
-        // Crea el patron para establecer los caracteres permitidos.
-        Pattern patternCadenaEntrada = Pattern.compile("([RGB])+");
-
-        // para garantizar que se ingresaron los caracteres adecuados.
-        Matcher matcher = patternCadenaEntrada.matcher(cadenaEntrada);
-
-        // Convierte la cadena ingresada en un arreglo de caracteres
-        char[] caracteres = cadenaEntrada.toCharArray();
-
-        // Si la validación del patron es exitosa entra a la función.
-        if (matcher.matches()) {
-
-            StringBuilder caracter = new StringBuilder();
-            // Recorre el arreglo de caracteres y va quitando la ultima posición al
-            // recorrerlo hasta que solo quede el ultimo caracter de la comparación.
-            for (int posicion = 0; posicion < (caracteres.length - 1); posicion++) {
-
-                if (caracteres.length == 1) {
-                    break;
-                }
-
-                char caracterValidado = validaColor(caracteres[posicion], caracteres[posicion + 1]);
-                caracteres[posicion] = caracterValidado;
-                caracter.append(caracterValidado);
-
-                //System.out.println(validaColor(caracteres[posicion], caracteres[posicion + 1]));
-                if ((posicion + 1) == caracteres.length) {
-                    posicion = 0;
-                    caracteres = caracter.toString().toCharArray();
-                    caracter = new StringBuilder();
-                }
+        while (pos > 1) {
+            for (int posicion = 0; posicion < (pos - 1); posicion++) {
+                caracteres[posicion] = validaColor(caracteres[posicion], caracteres[posicion + 1]);
             }
-        } else {
-            System.out.println("Entrada incorrecta, verifique que sus letras sean mayusculas y que contengan unicamente R, G ó B.");
+            pos--;
         }
         return caracteres[0];
     }
@@ -70,24 +30,23 @@ public class Kata {
      * @return Retorna el caracter que queda al realiar la evaluación necesaria.
      */
     private static char validaColor(char letraUno, char letraDos) {
-        char letraColor = ' ';
-
         if (letraUno == letraDos) {
             return letraUno;
         }
 
-        if (patternLetraG.matcher((letraUno + "" + letraDos)).matches()) {
+        String letras = letraUno + "" + letraDos;
+
+        if (letras.contains("R") && letras.contains("B")) {
             return 'G';
         }
 
-        if (patternLetraB.matcher((letraUno + "" + letraDos)).matches()) {
+        if (letras.contains("R") && letras.contains("G")) {
             return 'B';
         }
 
-        if (patternLetraR.matcher((letraUno + "" + letraDos)).matches()) {
+        if (letras.contains("G") && letras.contains("B")) {
             return 'R';
         }
-
-        return letraColor;
+        return letraUno;
     }
 }
